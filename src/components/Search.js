0193a0456fc3d7e401eae2f86725841d826830db
Swervosingh <153397884+Swervosingh@users.noreply.github.com>
2024-03-05 16:07:48 -0500

    Completed Plantshop

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Search() {
+function Search({onSearch}) {
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
@@ -8,10 +8,11 @@ function Search() {
         type="text"
         id="search"
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
+        onChange={onSearch}
       />
     </div>
   );
 }
 
 export default Search;
+
